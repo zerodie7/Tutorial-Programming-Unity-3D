bb82dfad93b710ee2feabbf0647917af01f2d80c
Diego Martinez <zerodie.7@hotmail.com>
2018-02-13 15:36:57 -0600

    Test

---
+++ ArraysScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArraysScript : MonoBehaviour {
+
+
+	string[] letras = new string[5]{"a","b","c","d","e"};
+	//                               0   1   2   3   4
+
+	public int[] numeros = new int[6]{0,1,2,3,4,5};
+
+
+	void Start () {
+
+		Debug.Log(letras[0]);
+		Debug.Log(letras[1]);
+		Debug.Log(letras[2]);
+		Debug.Log(letras[3]);
+		Debug.Log(letras[4]);
+		letras[0] = "z";
+		Debug.Log(letras[0]);
+
+	
+	}
+
+}
---
+++ Condicionales.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Condicionales : MonoBehaviour {
+
+	public int velocidadCoche;
+	private int velocidadMaxima = 120; 
+	private int velocidadMinima = 40;
+
+	void Update () {
+
+		if ( velocidadCoche > velocidadMaxima )
+		{
+			Debug.Log("Has superado la velocidad maxima");
+		}
+		else if( velocidadCoche < velocidadMinima )
+		{
+			Debug.Log("Estas en la velocidad minima");
+		}
+		else
+		{
+			Debug.Log("Estas entre la velocidad minima y maxima");
+		}
+
+		/*
+		 * A > B
+		 * A < B
+		 * A >= B
+		 * A <= B
+		 * A == B
+		 * A != B
+		 *    
+		 *   A > B & A < B  (y)
+		 *   A > B // A < B (o)
+		 * 
+		 * 
+		 * 
+		 * 
+		 * /
+		*/
+	
+	}
+}
